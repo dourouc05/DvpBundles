0604e30a3e8bc0a73a136d7ffdcc62de25dd3aab
Thibaut Cuvelier <dourouc05@gmail.com>
2012-08-31 16:12:34 +0200

    Admin for categories.

---
+++ src/Dvp/TeamBundle/Admin/CategoryAdmin.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Dvp\TeamBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Validator\ErrorElement;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+class CategoryAdmin extends Admin 
+{
+    // Show
+    protected function configureShowFields(ShowMapper $filter)
+    {
+        $filter
+            ->add('name')
+        ;
+    }
+
+    // Create, edit
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('name')
+        ;
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('name')
+        ;
+    }
+
+    // List
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('name')
+        ;
+    }
+}
