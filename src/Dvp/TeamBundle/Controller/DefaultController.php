bbdfea2c22f514153c3f4ae35beffead04d1a68c
Thibaut Cuvelier <dourouc05@gmail.com>
2012-08-31 16:30:14 +0200

    Linking things for the controller.

--- src/Dvp/TeamBundle/Controller/DefaultController.php
+++ src/Dvp/TeamBundle/Controller/DefaultController.php
@@ -6,8 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction($section)
     {
-        return $this->render('DvpTeamBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('DvpTeamBundle:Default:index.html.twig', array('name' => $section));
     }
 }
